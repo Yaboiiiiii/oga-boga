255bde35713bb5481e633ee26f51cee95ad4ef70
Emanuel Sorsén <emanuel.karlsson1@learnet.se>
2019-02-19 11:10:55 +0100

    123

---
+++ oga boga.py
@@ -0,0 +1,16 @@
+
+menu = int(input("Press 1 to generate a username and password \nPress 2 to read csv file \nPress 3 to edit csv file\nOption:"))
+
+if menu == 1:
+    print("ska den generera användarnamn/lösenord")
+
+elif menu == 2:
+    print("öppna csv filen för läsning")
+        
+elif menu ==3:
+    print("ska öppna csv filen för redigering")
+
+else:
+    print("Not a valid choice")
+
+     
